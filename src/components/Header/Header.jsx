05482ce776105739de7bf5a1baeb6757dbed0df4
abdullahrashid1638 <abdullahrashid1638@gmail.com>
2025-02-20 23:45:54 +0500

    *

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+
+  console.log(window.innerWidth, window.innerHeight);
+
+  return <nav></nav>;
+}
+
+export default Header;
